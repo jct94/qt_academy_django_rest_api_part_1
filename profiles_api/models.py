fa820f0910910c54cf4557f2cce5e1d112a1766d
jct94 <quantrack@pm.me>
2020-07-01 16:30:00 -0500

    patch user function

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -21,7 +21,7 @@ class UserProfileManager(BaseUserManager):
 
     def create_superuser(self, email, name, password=None):
         """Create new superuser profile"""
-        user = self.create_ser(email, name, password)
+        user = self.create_user(email, name, password)
 
         user.is_superuser = True
         user.is_staff = True
