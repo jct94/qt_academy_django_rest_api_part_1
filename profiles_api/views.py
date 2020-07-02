b4046b54a67ee8bff6a6cd429248123b8e526076
jct94 <quantrack@pm.me>
2020-07-02 09:19:57 -0500

    serialize APIview

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,10 +1,15 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status   #list of handy http status code, we will use it in our post function handler
+
+from profiles_api import serializers
+
 
 class HelloApiView(APIView):
     """
     Test API View method
     """
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         """
@@ -21,3 +26,17 @@ class HelloApiView(APIView):
         'message' : 'Hello',
         'an_apiview' : an_apiview
         } )
+
+    def post(self, request):
+        """
+        Create hello message with our name
+        """
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f"Hello {name}"
+            return Response({'message' : message})
+
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
