acc8bb9ad6519a86c992b9818a4ea2a491273262
jct94 <quantrack@pm.me>
2020-07-06 16:52:46 -0500

    add feed api permissions

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -136,7 +136,10 @@ class UserProfileFeedViewset(viewsets.ModelViewSet):
     authentication_classes = (TokenAuthentication,)
     serializer_class = serializers.ProfileFeedItemSerializer
     queryset = models.ProfileFeedItem.objects.all()
-
+    permission_classes = (
+    permissions.UpdateOwnStatus,
+    IsAuthenticatedOrReadOnly
+)
 
     def perform_create(self, serializer):
         """Sets the user profile to the logged in user"""
